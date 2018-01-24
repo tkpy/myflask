6136c11338f6f3113e2042d395cc33190be5a9da
tengkun <tengkun@zhugefang.com>
2018-01-24 21:31:10 +0800

    tk

--- hello.py
+++ hello.py
@@ -1,8 +1,13 @@
+# -*- coding: UTF-8 -*-
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return 'hello word'
+
+@app.route('/hello')
+def hello():
+	return '你好世界'
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=8008,debug=True)
