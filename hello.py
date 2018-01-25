14c9c215ca55819d9e090a5310bcf3b39a3e0800
tengkun <tengkun@zhugefang.com>
2018-01-25 11:40:12 +0800

    tk

--- hello.py
+++ hello.py
@@ -9,5 +9,8 @@ def index():
 @app.route('/hello')
 def hello():
 	return '你好世界'
+@app.route('user/<username>')
+def show_user_profile(username):
+	return "user %s"% username
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=8008,debug=True)
