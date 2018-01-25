220225509e3a2162b6b4659bf35cb8976ceaf407
tengkun <tengkun@zhugefang.com>
2018-01-25 14:07:44 +0800

    tk

--- hello.py
+++ hello.py
@@ -10,7 +10,7 @@ def index():
 def hello():
 	return '你好世界'
 
-# 携带参数
+携带参数
 @app.route('/user/<username>')
 def show_user_profile(username):
 	return "user %s"% username
@@ -27,6 +27,20 @@ def projects():
 @app.route('/about')
 def about():
 	return 'The about page'
+
+# 构造url
+from flask import url_for
+@app.route('/login')
+def login():
+	pass
+# @app.route('/user/<username>')
+# def profile(username):
+# 	pass
+with app.test_request_context():
+	print url_for('index')
+	print url_for('login')
+	print url_for('login',next='/')
+	print url_for('profile',userename='John Doe')
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=8008,debug=True)
