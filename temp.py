e3aa37c4fa43dd6ba839f7c265229e54773f31c4
tengkun <tengkun@zhugefang.com>
2018-01-27 12:13:03 +0800

    tk

--- temp.py
+++ temp.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/hello/')
 @app.route('/hello/<haha>')
 def hello(haha=None):
-	return render_template('hello.html',name=haha)
+	return render_template('hello.html',context=haha)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=8008,debug=True)
