9c39bd70b9828624314ce6cdf1d866897ce5fa7f
xuxianglei <xuxianglei@duozhuan.cn>
2018-07-20 17:04:01 +0800

    tk

--- steem1.py
+++ steem1.py
@@ -14,15 +14,15 @@ def bitalk_log(json):
 		print(p)
 	except Exception as e:
 		print(e)
-# json = {
-# 	"tags":"['cn','life']",
-# 	"content":"'来两张图片给大家'+'  '+'https://ipfs.busy.org/ipfs/QmdXEj3eyCX76MGaZRL8vMr45ts6xBRnqo9W6D7D2fuNtZ'+'  '+'https://ipfs.busy.org/ipfs/QmREWeH3kHQ4DXbS2DBxL3tmRkCkTkwzc6aCgmL4YPVA8r'",
-# 	"title":"'love'",
-# 	"author":"'changeday'",
-# 	"permlink":"'29qby5'",
-# 	'wif':'{"posting": "5JQznmiJ5qGqCk6LuKd1dXbkR4F2epm6VmqwGGRjPMfJVyggxNN"}',
-# }
-# bitalk_log(json)
+json = {
+	"tags":"['cn','life']",
+	"content":"'来两张图片给大家'+'  '+'https://ipfs.busy.org/ipfs/QmdXEj3eyCX76MGaZRL8vMr45ts6xBRnqo9W6D7D2fuNtZ'+'  '+'https://ipfs.busy.org/ipfs/QmREWeH3kHQ4DXbS2DBxL3tmRkCkTkwzc6aCgmL4YPVA8r'",
+	"title":"'love'",
+	"author":"'changeday'",
+	"permlink":"'29qby5'",
+	'wif':'{"posting": "5JQznmiJ5qGqCk6LuKd1dXbkR4F2epm6VmqwGGRjPMfJVyggxNN"}',
+}
+bitalk_log(json)
 if __name__ == '__main__':
 	json = {
 		"tags": "['cn','life']",
