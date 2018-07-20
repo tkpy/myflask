821acfaa3b53c7db6b4ceba50fa6f9bc54844616
xuxianglei <xuxianglei@duozhuan.cn>
2018-07-20 17:34:16 +0800

    tk

--- steem1.py
+++ steem1.py
@@ -18,7 +18,7 @@ def bitalk_log(json_dict):
 json_dict = {
 		"tags": ['cn','life'],
 		"content": '来两张图片给大家'+'  '+'https://ipfs.busy.org/ipfs/QmdXEj3eyCX76MGaZRL8vMr45ts6xBRnqo9W6D7D2fuNtZ'+'  '+'https://ipfs.busy.org/ipfs/QmREWeH3kHQ4DXbS2DBxL3tmRkCkTkwzc6aCgmL4YPVA8r',
-		"title": "上传图片试试",
+		"title": "haha",
 		"author": "changeday",
 		"permlink": "29qby5",
 		'wif': {"posting": "5JQznmiJ5qGqCk6LuKd1dXbkR4F2epm6VmqwGGRjPMfJVyggxNN"},
