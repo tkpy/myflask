350b6c9a9772a148c3a300c4411fee08df32d4c0
xuxianglei <xuxianglei@duozhuan.cn>
2018-07-20 18:10:33 +0800

    tk

--- steem1.py
+++ steem1.py
@@ -11,8 +11,12 @@ def bitalk_log(json_dict):
 		title = json_dict['title']
 		author = json_dict['author']
 		permlink = json_dict['permlink']
+		# 发帖
 		p = steem.post(title=title,body=content,author=author,tags=tags,permlink=permlink)
-		return p
+		# 回帖
+		s = steem.post(title='',body='这是我的回帖',author=author,reply_identifier='@{}/{}'.format(author,permlink))
+		res = {'p':p,'s':s}
+		return res
 	except Exception as e:
 		print(e)
 json_dict = {
@@ -20,19 +24,11 @@ json_dict = {
 		"content": '来两张图片给大家'+'  '+'https://ipfs.busy.org/ipfs/QmdXEj3eyCX76MGaZRL8vMr45ts6xBRnqo9W6D7D2fuNtZ'+'  '+'https://ipfs.busy.org/ipfs/QmREWeH3kHQ4DXbS2DBxL3tmRkCkTkwzc6aCgmL4YPVA8r',
 		"title": "python api test",
 		"author": "changeday",
-		"permlink": "111",
+		"permlink": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
 		'wif': {"posting": "5JQznmiJ5qGqCk6LuKd1dXbkR4F2epm6VmqwGGRjPMfJVyggxNN"},
 	}
-p = bitalk_log(json_dict)
-print(p)
-# if __name__ == '__main__':
-# 	json_dict = {
-# 		"tags": ['cn','life'],
-# 		"content": "'来两张图片给大家'+'  '+'https://ipfs.busy.org/ipfs/QmdXEj3eyCX76MGaZRL8vMr45ts6xBRnqo9W6D7D2fuNtZ'+'  '+'https://ipfs.busy.org/ipfs/QmREWeH3kHQ4DXbS2DBxL3tmRkCkTkwzc6aCgmL4YPVA8r'",
-# 		"title": "'love'",
-# 		"author": "'changeday'",
-# 		"permlink": "'29qby5'",
-# 		'wif': {"posting": "5JQznmiJ5qGqCk6LuKd1dXbkR4F2epm6VmqwGGRjPMfJVyggxNN"},
-# 	}
-# 	bitalk_log(json_dict)
+res = bitalk_log(json_dict)
+print('发帖返回内容',res['p'])
+print('回帖返回内容',res['s'])
+
 
