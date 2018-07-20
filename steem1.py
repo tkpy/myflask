89b2407f5d7d90c1016b1d6a5344fa16e0b72015
xuxianglei <xuxianglei@duozhuan.cn>
2018-07-20 17:24:08 +0800

    tk

--- steem1.py
+++ steem1.py
@@ -12,17 +12,19 @@ def bitalk_log(json_dict):
 		author = json_dict['author']
 		permlink = json_dict['permlink']
 		p = steem.post(title=title,body=content,author=author,tags=tags)
+		return p
 	except Exception as e:
 		print(e)
 json_dict = {
 		"tags": ['cn','life'],
 		"content": '来两张图片给大家'+'  '+'https://ipfs.busy.org/ipfs/QmdXEj3eyCX76MGaZRL8vMr45ts6xBRnqo9W6D7D2fuNtZ'+'  '+'https://ipfs.busy.org/ipfs/QmREWeH3kHQ4DXbS2DBxL3tmRkCkTkwzc6aCgmL4YPVA8r',
-		"title": "love",
+		"title": "上传图片试试",
 		"author": "changeday",
 		"permlink": "29qby5",
 		'wif': {"posting": "5JQznmiJ5qGqCk6LuKd1dXbkR4F2epm6VmqwGGRjPMfJVyggxNN"},
 	}
-bitalk_log(json_dict)
+p = bitalk_log(json_dict)
+print(p)
 # if __name__ == '__main__':
 # 	json_dict = {
 # 		"tags": ['cn','life'],
