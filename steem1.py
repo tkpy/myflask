17d15c0a6b0394b9d4b5c5a868ff250a397a76d1
xuxianglei <xuxianglei@duozhuan.cn>
2018-07-20 14:23:38 +0800

    tk

--- steem1.py
+++ steem1.py
@@ -4,8 +4,13 @@ wif = {
 	"posting": "5JQznmiJ5qGqCk6LuKd1dXbkR4F2epm6VmqwGGRjPMfJVyggxNN",
 }
 steem = Steem(keys=wif)
+# 主题
 tags = ['cn','life']
-content = 'l love life'
+# 内容
+with open('./liangzijinrong.jpg','r') as fp:
+	s = fp.read()
+	content = s
+# 标题
 title = 'love'
 p = steem.post(title,content,author='changeday',tags=tags)
 print(p)
