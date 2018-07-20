38c62afd8f88a1bf5dd789cbc7a2f9e30e3d5555
xuxianglei <xuxianglei@duozhuan.cn>
2018-07-20 17:16:44 +0800

    tk

--- steem1.py
+++ steem1.py
@@ -1,12 +1,5 @@
 from steem import Steem
-json_dict = {
-	"tags":"['cn','life']",
-	"content":"'来两张图片给大家'+'  '+'https://ipfs.busy.org/ipfs/QmdXEj3eyCX76MGaZRL8vMr45ts6xBRnqo9W6D7D2fuNtZ'+'  '+'https://ipfs.busy.org/ipfs/QmREWeH3kHQ4DXbS2DBxL3tmRkCkTkwzc6aCgmL4YPVA8r'",
-	"title":"'love'",
-	"author":"'changeday'",
-	"permlink":"'29qby5'",
-	'wif':'{"posting": "5JQznmiJ5qGqCk6LuKd1dXbkR4F2epm6VmqwGGRjPMfJVyggxNN"}',
-}
+
 def bitalk_log(json_dict):
 	try:
 		wif = json_dict['wif']
@@ -19,18 +12,17 @@ def bitalk_log(json_dict):
 		author = json_dict['author']
 		permlink = json_dict['permlink']
 		p = steem.post(title=title,body=content,author=author,tags=tags)
-		print(p)
 	except Exception as e:
 		print(e)
 
 if __name__ == '__main__':
 	json_dict = {
-		"tags": "['cn','life']",
+		"tags": ['cn','life'],
 		"content": "'来两张图片给大家'+'  '+'https://ipfs.busy.org/ipfs/QmdXEj3eyCX76MGaZRL8vMr45ts6xBRnqo9W6D7D2fuNtZ'+'  '+'https://ipfs.busy.org/ipfs/QmREWeH3kHQ4DXbS2DBxL3tmRkCkTkwzc6aCgmL4YPVA8r'",
 		"title": "'love'",
 		"author": "'changeday'",
 		"permlink": "'29qby5'",
-		'wif': '{"posting": "5JQznmiJ5qGqCk6LuKd1dXbkR4F2epm6VmqwGGRjPMfJVyggxNN"}',
+		'wif': {"posting": "5JQznmiJ5qGqCk6LuKd1dXbkR4F2epm6VmqwGGRjPMfJVyggxNN"},
 	}
 	bitalk_log(json_dict)
 
